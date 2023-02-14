d98586b68f78717e0ea3697c44bc678f2b6385e1
danielmedina1 <dam_danielmedina@riberadeltajo.es>
2023-02-14 16:01:01 +0100

    Un syso mas en la clase Principal

--- proyecto1/src/paquete/Principal.java
+++ proyecto1/src/paquete/Principal.java
@@ -3,6 +3,7 @@ package paquete;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("El cambio a la clase Principal");
 	}
 	
 	
